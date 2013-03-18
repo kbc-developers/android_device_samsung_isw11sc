d6a52ed0fc311709e1283711819476b8777697bc
Dorian Snyder <dastin1015@gmail.com>
2013-03-18 00:10:51 -0700

    Revert "d710: update audio_policy.conf"
    This reverts commit 60384e0999a6c6174b384a9f7520d2a7c9ae176e.

--- d710.mk
+++ d710.mk
@@ -55,8 +55,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf
 
 # Packages
 PRODUCT_PACKAGES := \
